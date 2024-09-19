4375aea79728a5f35411a78050ac80706aec0904
Angelina Borgoyakova <a.borgoiakova@bars.group>
2024-09-19 19:24:54 +0700

    Изменена логика файла Main, добавлена обработка исключений

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int count = 1;
         while(true){
             System.out.println("Введите путь к файлу и нажмите <Enter>:");
@@ -18,6 +18,32 @@ public class Main {
             else {
                 System.out.println("Указанный файл не существует или указанный путь является путем к папке");
                 continue;
+            }
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+                int minLength = 0;
+                int maxLength = 0;
+                int sumString = 0;
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    if (length > 1024){
+                        throw new RuntimeException("Длина строки превысило 1024 символов");
+                    }
+                    if (length > maxLength) maxLength = length;
+                    if (sumString == 0 || length < minLength) minLength = length;
+                    sumString+=1;
+                }
+                System.out.println("Общее количество строк в файле " + sumString);
+                System.out.println("Длина самой длинной строки в файле " + maxLength);
+                System.out.println("Длина самой короткой строки в файле " + minLength);
+            }
+            catch (FileNotFoundException ex){
+                ex.printStackTrace();
+            }
+            catch (IOException ex){
+                ex.printStackTrace();
             }
         }
     }
